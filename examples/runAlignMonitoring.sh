1eed95353f75b420773a10969713c0384c2c2763
Pierfrancesco Butti <pierfrancesco.butti@gmail.com>
2020-05-01 17:11:46 -0700

    Update to runAlignMonitoring

--- examples/runAlignMonitoring.sh
+++ examples/runAlignMonitoring.sh
@@ -1,17 +1,21 @@
 iteration=$1
 tag=$2
+datainfo=$3
+listfiles=$4
+runNumber=$5
 
-if [ "$#" -ne 2 ]; then
+if [ "$#" -ne 5 ]; then
     echo "Illegal number of parameters"
+    echo "runAlignmMonitoring.sh <iteration> <tag> <dataInfo> <listfiles> <runNumber>"
     exit 1
     fi
 
-python scripts/submit_jobs.py --outdir ~/10031_AlignmentMonitoring_v0Skims_MPIIdata_${tag}_$iteration/ --listfiles 10031_data_2019_v0skims.txt --step=recon --fileExt slcio --nevents -1 --isData 1 --year=2019 --extraFlags="-R 10031" --steeringFile "/nfs/slac/g/hps2/pbutti/alignment/hps-java/gbl_alignFromLCIO_newGeo.lcsim" --tmpPrefix ~/scratch  -d HPS_${tag}_$iteration 
+python scripts/submit_jobs.py --outdir ~/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/ --listfiles ${listfiles} --step=recon --fileExt slcio --nevents -1 --isData 1 --year=2019 --extraFlags="-R $runNumber" --steeringFile "/nfs/slac/g/hps2/pbutti/alignment/hps-java/gbl_alignFromLCIO_newGeo.lcsim" --tmpPrefix ~/scratch  -d HPS_${tag}_$iteration 
 
-ls -1 --color=never ~/10031_AlignmentMonitoring_v0Skims_MPIIdata_${tag}_$iteration/submit_scripts/*.sh > list${tag}_$iteration.txt
-echo "python ./scripts/run_shPool.py --fileList list${tag}_$iteration.txt --logDir ~/10031_AlignmentMonitoring_v0Skims_MPIIdata_${tag}_$iteration/Logs/ -p 10"
+ls -1 --color=never ~/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/submit_scripts/*.sh > list${tag}_$iteration.txt
+echo "python ./scripts/run_shPool.py --fileList list${tag}_$iteration.txt --logDir ~/10031_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/Logs/ -p 10"
 
-python ./scripts/run_shPool.py --fileList list${tag}_$iteration.txt --logDir ~/10031_AlignmentMonitoring_v0Skims_MPIIdata_${tag}_$iteration/Logs/ -p 10
+python ./scripts/run_shPool.py --fileList list${tag}_$iteration.txt --logDir ~/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/Logs/ -p 10
 
-cd ~/10031_AlignmentMonitoring_v0Skims_MPIIdata_${tag}_$iteration/outputFiles
-hadd AlignMonitoring_10031.root output*/*.root
+cd ~/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/outputFiles
+hadd AlignMonitoring_${runNumber}_${tag}_${iteration}.root output*/*.root
