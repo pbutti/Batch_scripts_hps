40b064f72f4939845a8cd274dc6321f21691d1ad
Pierfrancesco Butti <pierfrancesco.butti@gmail.com>
2020-05-05 18:26:20 -0700

    Help processing for alignment

--- examples/runAlignMonitoring.sh
+++ examples/runAlignMonitoring.sh
@@ -3,20 +3,23 @@ tag=$2
 datainfo=$3
 listfiles=$4
 runNumber=$5
+basepath=$6
 
-if [ "$#" -ne 5 ]; then
+if [ "$#" -ne 6 ]; then
     echo "Illegal number of parameters"
-    echo "runAlignmMonitoring.sh <iteration> <tag> <dataInfo> <listfiles> <runNumber>"
+    echo "runAlignmMonitoring.sh <iteration> <tag> <dataInfo> <listfiles> <runNumber> <basepath ~/ | /nfs/slac/g/hps2/pbutti/>"
     exit 1
-    fi
+fi
 
-python scripts/submit_jobs.py --outdir ~/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/ --listfiles ${listfiles} --step=recon --fileExt slcio --nevents -1 --isData 1 --year=2019 --extraFlags="-R $runNumber" --steeringFile "/nfs/slac/g/hps2/pbutti/alignment/hps-java/gbl_alignFromLCIO_newGeo.lcsim" --tmpPrefix ~/scratch  -d HPS_${tag}_$iteration 
 
-ls -1 --color=never ~/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/submit_scripts/*.sh > list${tag}_$iteration.txt
-echo "python ./scripts/run_shPool.py --fileList list${tag}_$iteration.txt --logDir ~/10031_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/Logs/ -p 10"
 
-python ./scripts/run_shPool.py --fileList list${tag}_$iteration.txt --logDir ~/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/Logs/ -p 10
+python scripts/submit_jobs.py --outdir ${basepath}/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/ --listfiles ${listfiles} --step=recon --fileExt slcio --nevents -1 --isData 1 --year=2019 --extraFlags="-R $runNumber" --steeringFile "/nfs/slac/g/hps2/pbutti/alignment/hps-java/gbl_alignFromLCIO_newGeo.lcsim" --tmpPrefix ~/scratch  -d HPS_${tag}_$iteration 
 
-cd ~/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/outputFiles
+ls -1 --color=never ${basepath}/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/submit_scripts/*.sh > list${tag}_$iteration.txt
+echo "python ./scripts/run_shPool.py --fileList list${tag}_$iteration.txt --logDir ${basepath}/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/Logs/ -p 10"
+
+python ./scripts/run_shPool.py --fileList list${tag}_$iteration.txt --logDir ${basepath}/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/Logs/ -p 10
+
+cd ${basepath}/${runNumber}_AlignmentMonitoring_${datainfo}_MPIIdata_${tag}_$iteration/outputFiles
 hadd AlignMonitoring_${runNumber}_${tag}_${iteration}.root output*/*.root
 
--- examples/runAlignMonitoring_MC.sh
+++ examples/runAlignMonitoring_MC.sh
@@ -1,22 +1,28 @@
 iteration=$1
+tag=$2
 
-if [ "$#" -ne 1 ]; then
+if [ "$#" -ne 2 ]; then
     echo "Illegal number of parameters"
+    echo "Usage: runAlignMonitoring_MC.py <iter> <tag>"
     exit 1
 fi
 
-python scripts/submit_jobs.py --outdir ~/TriTrig_AlignmentMonitoring_MPIIdata_AliTest_$iteration/ --listfiles triTrig_2019_nobeam_readout.txt --step=recon --fileExt slcio --nevents -1 --isData 0 --year=2019 --extraFlags="-R 10666" --steeringFile "/nfs/slac/g/hps2/pbutti/alignment/hps-java/gbl_alignReadout_newGeo.lcsim" --tmpPrefix ~/scratch  -d HPS_AliTest_$iteration 
+python scripts/submit_jobs.py --outdir ~/TriTrig_AlignmentMonitoring_MPIIdata_${tag}_$iteration/ --listfiles triTrig_2019_nobeam_readout.txt --step=recon --fileExt slcio --nevents -1 --isData 0 --year=2019 --extraFlags="-R 10666" --steeringFile "/nfs/slac/g/hps2/pbutti/alignment/hps-java/gbl_alignReadout_newGeo.lcsim" --tmpPrefix ~/scratch  -d HPS_${tag}_$iteration 
 
-ls -1 --color=never ~/TriTrig_AlignmentMonitoring_MPIIdata_AliTest_$iteration/submit_scripts/*.sh > triTrig_2019_nobeam_$iteration.txt
-echo "python ./scripts/run_shPool.py --fileList triTrig_2019_nobeam_$iteration.txt --logDir ~/TriTrig_AlignmentMonitoring_MPIIdata_AliTest_$iteration/Logs/ -p 10"
+ls -1 --color=never ~/TriTrig_AlignmentMonitoring_MPIIdata_${tag}_$iteration/submit_scripts/*.sh > triTrig_2019_nobeam_${tag}_$iteration.txt
+echo "python ./scripts/run_shPool.py --fileList triTrig_2019_nobeam_${tag}_$iteration.txt --logDir ~/TriTrig_AlignmentMonitoring_MPIIdata_${tag}_$iteration/Logs/ -p 10"
 
-python ./scripts/run_shPool.py --fileList triTrig_2019_nobeam_$iteration.txt --logDir ~/TriTrig_AlignmentMonitoring_MPIIdata_AliTest_$iteration/Logs/ -p 10
+python ./scripts/run_shPool.py --fileList triTrig_2019_nobeam_${tag}_$iteration.txt --logDir ~/TriTrig_AlignmentMonitoring_MPIIdata_${tag}_$iteration/Logs/ -p 10
 
 
 echo "Adding the monitoring histogram..."
-cd ~/TriTrig_AlignmentMonitoring_MPIIdata_AliTest_$iteration/outputFiles
+cd ~/TriTrig_AlignmentMonitoring_MPIIdata_${tag}_$iteration/outputFiles
 hadd AlignMonitoring_10031.root output*/*.root
 
 #Clean the LCIOs
 echo "Cleaning slcios...."
 rm output*/*.slcio
+
+#Clean the root files
+echo "Cleaning the root files...."
+rm output*/*.root
